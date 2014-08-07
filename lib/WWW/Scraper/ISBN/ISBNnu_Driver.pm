09d34d78bee783720c4c81c2631ab8abcf758b84
Barbie <barbie@missbarbell.co.uk>
2014-08-07 02:05:20 +0000

    Import of BARBIE/WWW-Scraper-ISBN-ISBNnu_Driver-0.23 from CPAN.
    gitpan-cpan-distribution: WWW-Scraper-ISBN-ISBNnu_Driver
    gitpan-cpan-version:      0.23
    gitpan-cpan-path:         BARBIE/WWW-Scraper-ISBN-ISBNnu_Driver-0.23.tar.gz
    gitpan-cpan-author:       BARBIE
    gitpan-cpan-maturity:     released

--- lib/WWW/Scraper/ISBN/ISBNnu_Driver.pm
+++ lib/WWW/Scraper/ISBN/ISBNnu_Driver.pm
@@ -3,7 +3,7 @@ package WWW::Scraper::ISBN::ISBNnu_Driver;
 use strict;
 use warnings;
 
-our $VERSION = '0.22';
+our $VERSION = '0.23';
 
 #--------------------------------------------------------------------------
 
